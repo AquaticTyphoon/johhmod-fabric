022284b7cc438b943d4a6afa761111c38a275833
AquaticTyphoon <aquatictyphoon@yahoo.com>
2023-01-11 10:29:21 -0600

    Initial commit

---
+++ src/main/java/net/aquatic/johnmod/JohnMod.java
@@ -0,0 +1,13 @@
+package net.aquatic.johnmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JohnMod implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("johnmod");
+
+	@Override
+	public void onInitialize() {
+	}
+}
